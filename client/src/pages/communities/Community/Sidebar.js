64467cb4bb18ce0cda715415e4138d13082977ff
mayinx <mayinxx@web.de>
2021-08-23 02:22:57 +0200

    [Client] Add community user group dummies to CurrentCommunity-Sidebar-layout

--- client/src/pages/communities/Community/Sidebar.js
+++ client/src/pages/communities/Community/Sidebar.js
@@ -8,7 +8,36 @@ export default function Sidebar(props) {
         <CommunityProfile community={props.community} />
       </div>
 
-      <div className="SidebarContent"></div>
+      <div className="SidebarContent">
+        <ol className="CommunityGroupsList defaultGroups">
+          <li>
+            <span className="groupName">Community</span>
+          </li>
+          <li>
+            <span className="groupName">Students</span>
+          </li>
+          <li>
+            <span className="groupName">Teachers</span>
+          </li>
+          <li>
+            <span className="groupName">Parents</span>
+          </li>
+        </ol>
+        <ol className="CommunityGroupsList customGroups">
+          <li>
+            <span className="groupName">Mathe</span>
+          </li>
+          <li>
+            <span className="groupName">Deutsch</span>
+          </li>
+          <li>
+            <span className="groupName">Englisch</span>
+          </li>
+          <li>
+            <span className="groupName">Informatik</span>
+          </li>
+        </ol>
+      </div>
     </sidebar>
   );
 }
--- client/src/pages/communities/CommunityPage.js
+++ client/src/pages/communities/CommunityPage.js
@@ -1,6 +1,7 @@
 // import Community from "../../domain/Community/Community.js";
 import CommunityContext from "../../contexts/CommunityContext.js";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { useMediaQuery } from "react-responsive";
 
 import "./CommunityPage.css";
 
@@ -11,21 +12,87 @@ import "./Community/media-queries.css";
 export default function CommunityPage() {
   const { currentCommunity } = useContext(CommunityContext);
   // states: hidden - collapsed - expanded
-  const [toggled, setToggled] = useState(false);
+  const [sidebarToggled, setSidebarToggled] = useState(false);
+  const [mediaQueryToggled, setMediaQueryToggled] = useState(false);
+  const [sidebarStateClass, setSidebarStateClass] = useState(
+    "CommunitySidebar"
+  );
+
+  const handleMediaQueryChange = (matches) => {
+    // matches will be true or false based on the value for the media query
+    console.log("Media Query Change");
+
+    setMediaQueryToggled(!mediaQueryToggled);
+  };
+
+  const isSmallMobile = useMediaQuery(
+    // { query: "(max-width: 399px)" }
+    { maxWidth: 399 },
+    undefined,
+    handleMediaQueryChange
+  );
+
+  /* from here on (up until min-width: 992px / i.e. large screens)
+   the sidebar is always visible:
+   - default / untoggled: sidebar collapsed to (always visible) min bar
+   - toggled: sidebar expanded to max width
+*/
+  const isBiggerMobile = useMediaQuery(
+    // { query: "(min-width: 400px)" }
+    { minWidth: 400, maxWidth: 991 },
+
+    undefined,
+    handleMediaQueryChange
+  );
+
+  /* from here on the sidebar is always visible as well - but:
+   - default / untoggled: sidebar expanded to max width per default
+   - toggled: collapsed to (always visible) min bar
+*/
+  const isDesktop = useMediaQuery(
+    // { query: "(min-width: 992px)" }
+    { minWidth: 992 },
+    undefined,
+    handleMediaQueryChange
+  );
 
   // TODO: REFACTORING: Check responsive react hook or something to produce
   // three different state-specific css-classes here:
   // 'CommunitySidebar--hidden', 'CommunitySidebar--collaped' + 'CommunitySidebar--expanded'
   const toggleSidebar = () => {
-    setToggled(!toggled);
+    setSidebarToggled(!sidebarToggled);
   };
+
+  useEffect(() => {
+    let sidebarClass = "CommunitySidebar";
+    console.log("useEffect");
+    console.log("toggled", sidebarToggled);
+    console.log("isSmallMobile", isSmallMobile);
+    console.log("isBiggerMobile", isBiggerMobile);
+    console.log("isDesktop", isDesktop);
+
+    if (isSmallMobile) {
+      sidebarClass += sidebarToggled
+        ? " CommunitySidebar--toggled CommunitySidebar--expanded"
+        : " CommunitySidebar--hidden";
+    } else if (isBiggerMobile) {
+      sidebarClass += sidebarToggled
+        ? " CommunitySidebar--toggled CommunitySidebar--expanded"
+        : " CommunitySidebar--collapsed";
+    } else if (isDesktop) {
+      sidebarClass += sidebarToggled
+        ? " CommunitySidebar--toggled CommunitySidebar--collapsed"
+        : " CommunitySidebar--expanded";
+    }
+
+    console.log("new sidebarClass: ", sidebarClass);
+    setSidebarStateClass(sidebarClass);
+  }, [sidebarToggled, mediaQueryToggled]);
 
   return (
     <>
       <CommunitySidebar
-        className={`CommunitySidebar ${
-          toggled ? "CommunitySidebar--toggled" : null
-        }`}
+        className={sidebarStateClass}
         community={currentCommunity}
       />
 

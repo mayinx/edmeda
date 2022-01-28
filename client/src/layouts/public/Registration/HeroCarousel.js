f0b68f1e12ff7c6a9bab4163b519cb3dc06d4662
mayinx <mayinxx@web.de>
2022-01-28 19:47:27 +0100

    Fix responsiveness issues with coarousel legend

--- client/src/layouts/public/Registration/HeroCarousel.js
+++ client/src/layouts/public/Registration/HeroCarousel.js
@@ -10,77 +10,81 @@ import CarouselPic5 from "../../../assets/shutterstock_165821084.jpg";
 import CarouselPic6 from "../../../assets/SaferinternetatOIAT_1.jpg";
 import CarouselPic7 from "../../../assets/shutterstock_1684741660.jpg";
 
-export default () => (
-  <Carousel className="HeroCarousel" autoPlay infiniteLoop showThumbs={false}>
-    {/* <Carousel autoPlay dynamicHeight={true}> */}
-    <div>
-      <img alt="" src={CarouselPic1} />
-      <div className="legend">
-        <div className="inner">
-          <h1>Edmeda</h1>
-          <p>Easy remote communication & collaboration for Schools!</p>
+export default function HeroCarousel() {
+  // <Carousel className="HeroCarousel" autoPlay infiniteLoop showThumbs={false}>
+  return (
+    <Carousel className="HeroCarousel" showThumbs={false}>
+      {/* <Carousel autoPlay dynamicHeight={true}> */}
+      <div>
+        <img alt="" src={CarouselPic1} />
+        <div className="legend">
+          <div className="inner">
+            <h1>Edmeda</h1>
+            <p>Easy remote communication & collaboration for Schools!</p>
+          </div>
         </div>
       </div>
-    </div>
-    <div>
-      <img alt="" src={CarouselPic2} />
-      <div className="legend">
-        <div className="inner">
-          <h1>Communities</h1>
-          <p>Limitless communities for Schools - limitless possibilities!</p>
+      <div>
+        <img alt="" src={CarouselPic2} />
+        <div className="legend">
+          <div className="inner">
+            <h1>Communities</h1>
+            <p>Limitless communities for Schools - limitless possibilities!</p>
+          </div>
         </div>
       </div>
-    </div>
-    <div>
-      <img alt="" src={CarouselPic3} />
-      <div className="legend">
-        <div className="inner">
-          <h1>User Groups</h1>
-          <p style={{ width: "350px" }}>
-            Each community consists of multiple user groups - for every
-            occasion: School-subjects, Students, Parents, Teachers...
-          </p>
+      <div>
+        <img alt="" src={CarouselPic3} />
+        <div className="legend">
+          <div className="inner">
+            <h1>User Groups</h1>
+            <p>
+              Each community consists of multiple user groups - for every
+              occasion: School-subjects, Students, Parents, Teachers...
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-    <div>
-      <img alt="" src={CarouselPic4} />
-      <div className="legend">
-        <div className="inner">
-          <h1>Group Chats</h1>
-          <p style={{ width: "350px" }}>
-            Each user group is equipped with its own Group Chat - thus enabling
-            the members to communicate and collaborate efficiently as a team!
-          </p>
+      <div>
+        <img alt="" src={CarouselPic4} />
+        <div className="legend">
+          <div className="inner">
+            <h1>Group Chats</h1>
+            <p>
+              Each user group is equipped with its own Group Chat - thus
+              enabling the members to communicate and collaborate efficiently as
+              a team!
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-    <div>
-      <img alt="" src={CarouselPic5} />
-      <div className="legend">
-        <div className="inner">
-          <h1>Edmeda</h1>
-          <p>Easy remote communication & collaboration for Schools!</p>
+      <div>
+        <img alt="" src={CarouselPic5} />
+        <div className="legend">
+          <div className="inner">
+            <h1>Edmeda</h1>
+            <p>Easy remote communication & collaboration for Schools!</p>
+          </div>
         </div>
       </div>
-    </div>
-    <div>
-      <img alt="" src={CarouselPic6} />
-      <div className="legend">
-        <div className="inner">
-          <h1>Edmeda</h1>
-          <p>Easy remote communication & collaboration for Schools!</p>
+      <div>
+        <img alt="" src={CarouselPic6} />
+        <div className="legend">
+          <div className="inner">
+            <h1>Edmeda</h1>
+            <p>Easy remote communication & collaboration for Schools!</p>
+          </div>
         </div>
       </div>
-    </div>
-    <div>
-      <img alt="" src={CarouselPic7} />
-      <div className="legend">
-        <div className="inner">
-          <h1>Edmeda</h1>
-          <p>Easy remote communication & collaboration for Schools!</p>
+      <div>
+        <img alt="" src={CarouselPic7} />
+        <div className="legend">
+          <div className="inner">
+            <h1>Edmeda</h1>
+            <p>Easy remote communication & collaboration for Schools!</p>
+          </div>
         </div>
       </div>
-    </div>
-  </Carousel>
-);
+    </Carousel>
+  );
+}
